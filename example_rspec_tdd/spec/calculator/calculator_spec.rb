77c56f6de5d2e4967d23ac6986d33dd9f89438f1
Tulio Fractal <tulioteodoromanso@gmail.com>
2023-03-03 10:54:07 -0300

    It, xit, test specific line or 'it'

--- example_rspec_tdd/spec/calculator/calculator_spec.rb
+++ example_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -1,25 +1,25 @@
 require 'calculator'
 
 describe Calculator do
-
-    subject(:calc) { described_class.new() } # or you can change the describe 'ChangeToText'
-                                            # and change 'described_class' to the name of the class,
-                                            # this is to we dont need use 'subject.sum' method, but call the real name method 'calc'.
-    
     context '#sum' do
         it 'with positive numbers' do
-            result = calc.sum(5, 7)
+            result = subject.sum(5, 7)
             expect(result).to eq(12)
         end
 
         it 'with negative and positive numbers' do
-            result = calc.sum(-5, 7)
+            result = subject.sum(-5, 7)
             expect(result).to eq(2)
         end
 
-        it 'with negative numbers' do
-            result = calc.sum(-5, -7)
+        it 'with negative numbers' do #, puts 'x' in the it, 'xit', to put this pending. Or let the corpse of the 'it...' empty.
+            result = subject.sum(-5, -7)
             expect(result).to eq(-12)
         end
+
+        it 'with numbers'
     end
 end
+
+# To load only a specif test, you can use "rspec folder/folder2/file.rb -e 'text inside the it'"
+# or, to execut by the line of 'it' you want: "rspec folder/folder2/file.rb:numberLineIt"
