24ec0e54b2e15d0ca185c8770905e5c5b9536283
TulioTMWsl2 <tulioteodoromanso@gmail.com>
2023-03-07 19:58:16 -0300

    Tag Filters, important, lesson 41

--- example_rspec_tdd/spec/matchers/arrays/arrays_with_negatives_matchers_spec.rb
+++ example_rspec_tdd/spec/matchers/arrays/arrays_with_negatives_matchers_spec.rb
@@ -1,6 +1,6 @@
 RSpec::Matchers.define_negated_matcher :exclude, :include
 
-describe Array.new([1, 2, 3]), "Array" do
+describe Array.new([1, 2, 3]), "Array", type: 'collection' do # Another way to use Tag Filters, in terminal we use "rspec . -t type:collection"
     it '#include' do # 'include' only works for separated elements of the array
         expect(subject).to include(2)
         expect(subject).to include(2, 1)
@@ -12,7 +12,7 @@ describe Array.new([1, 2, 3]), "Array" do
         expect(subject).to match_array([2, 3, 1]) # its for exactly elements of array
     end
 
-    it '#contain_exactly' do
+    it '#contain_exactly', :slow do # In '.rspec' we write the '~' that will exlude all ":slow" tests 
         expect(subject).to contain_exactly(1, 2, 3) # its for exactly elements of array
     end
 end
