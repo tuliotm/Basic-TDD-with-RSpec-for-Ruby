aa9963fb0f3466207f3220e5b4b4ea9990e25329
TulioTMWsl2 <tulioteodoromanso@gmail.com>
2023-03-07 21:43:43 -0300

    Another Stub Methods

--- example_rspec_tdd/spec/stubs/stubs_spec.rb
+++ example_rspec_tdd/spec/stubs/stubs_spec.rb
@@ -12,6 +12,42 @@ describe 'Stub' do
     
         expect(course_finished).to be_truthy
     end
+
+    it 'Dinamics Arguments' do
+        student = Student.new
+
+        allow(student).to receive(:foo) do |arg|
+            if arg == :hello
+                "hellow"
+            elsif arg == :hi
+                "Hi!!!"
+            end
+        end
+
+        expect(student.foo(:hello)).to eq("hellow")
+        expect(student.foo(:hi)).to eq("Hi!!!")
+
+    end
+
+    it 'Any Class instance' do
+        student = Student.new
+        other_student = Student.new
+
+        allow_any_instance_of(Student).to receive(:bar).and_return(true)
+    
+        expect(student.bar).to be_truthy
+        expect(other_student.bar).to be_truthy
+    end
+
+    it 'Errors' do # Testing errors
+        student = Student.new
+
+        allow(student).to receive(:bar).and_raise(RuntimeError)
+    
+        expect{ student.bar }.to raise_error(RuntimeError) # We need to pay attention, because when we test errors,
+        # we need to change the 'expect(...)' to 'expect{...}'
+    end
+
 end
 
 
