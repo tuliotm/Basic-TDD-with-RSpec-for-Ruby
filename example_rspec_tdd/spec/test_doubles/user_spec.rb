e4f1b26edca477bc51079af320f3285285128ad5
TulioTMWsl2 <tulioteodoromanso@gmail.com>
2023-03-07 22:06:41 -0300

    Method 'As Null Object'

--- example_rspec_tdd/spec/test_doubles/user_spec.rb
+++ example_rspec_tdd/spec/test_doubles/user_spec.rb
@@ -8,4 +8,15 @@ describe 'Test Double' do
         puts user.name
         puts user.password
     end
+
+    it 'as_null_object' do
+        user = double('User').as_null_object # with this method, any call to "double" that brings a error mensage, it will be transformed in null object
+        allow(user).to receive_messages(name: 'Tulio', password: 'secret')
+        # or
+        # allow(user).to receive(:name).and_return('Tulio')
+        # allow(user).to receive(:password).and_return('secret')
+        puts user.name
+        puts user.password
+        user.abc
+    end
 end
